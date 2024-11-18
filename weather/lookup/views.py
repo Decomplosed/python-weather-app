f33577066f039b86a54e39464e6ed9021219dfb3
Bart Frackiewicz <decomplosed@gmail.com>
2024-11-18 16:13:57 +0100

    Use python-dotenv

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -1,6 +1,12 @@
+import json
+import os
+import requests
 from django.shortcuts import render
 
+
 def home(request):
+    url = f"https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=89129&date=2024-11-18&distance=5&API_KEY={os.getenv('API_KEY')}"
+    api_request = requests.get("")
     return render(request, 'home.html', {})
 
 def about(request):
